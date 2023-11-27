12579ddae8d18611bed3bd728a656280bf0bb86a
Suraj_Kolhe <surajkolhe214@gmail.com>
2023-11-27 16:24:47 +0530

    apollo server logger added , logger added

--- src/apolloServer.ts
+++ src/apolloServer.ts
@@ -1,30 +1,28 @@
-import { ApolloServer } from "@apollo/server";
-import { ApolloServerPlugin } from "@apollo/server/dist/esm/externalTypes/plugins";
-
+import { ApolloServer , ApolloServerPlugin, BaseContext} from "@apollo/server";
+import logger from './config/logger';
 import typeDefs from './typeDefs';
 import resolver from './resolvers';
 
-type BaseContext = {};
 
 const plugin : ApolloServerPlugin<BaseContext> = {
-    async serverWillStart(requestContext) {
-        console.log('Apollo server start!');
+    async serverWillStart(_requestContext) {
+        logger.info('Apollo server start!');
         return;
     },
     
-    // Fires whenever a GraphQL request is received from a client.
-    async requestDidStart(requestContext) {
-        console.log('Request started!');
+    // // Fires whenever a GraphQL request is received from a client.
+    // async requestDidStart(requestContext) {
+    //     console.log('Request started!');
 
-        return ;
-    },
+    //     return ;
+    // },
 };
 
 // Create apollo/GraphQL server
 const graphQLServer = new ApolloServer({
     typeDefs: typeDefs,     // schema
     resolvers: resolver,    // resolvers
-    plugins: [plugin],
+    plugins: [plugin],      // plugin ref: https://www.apollographql.com/docs/apollo-server/integrations/plugins
 });
 
 
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,40 @@
 import express from 'express';
 import { expressMiddleware } from '@apollo/server/express4';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
 import graphQLServer from './apolloServer';
+import env from './config/config';
+import logger from './config/logger';
+import morgan from './config/morgan';
 
 
 async function startServer() {
 
-    // Create express app
-    const PORT = Number(process.env.PORT || 8080);
+    // Create express ap
     const app = express();
 
     app.use(express.json());
+    // Set necessary HTTP headers for app security
+    app.use(helmet());
+
+    // JSON requests are received as plain text. We need to parse the json request body.
+    app.use(express.json());
+
+    // Parse urlencoded request body if provided with any of the requests
+    app.use(express.urlencoded({ extended: true }));
+
+    // Using gzip compression for faster transfer of response data
+    app.use(compression());
+
+    // Enable cors to accept requests from any frontend domain, all possible HTTP methods, and necessary items in request headers
+    app.use(cors());
+    app.options('*', cors());
+
+    if (env.env !== 'test') {
+        app.use(morgan.successHandler);
+        app.use(morgan.errorHandler);
+    }
 
 
 
@@ -20,7 +45,7 @@ async function startServer() {
     app.use('/graphql', expressMiddleware(graphQLServer));
 
     // Start express app 
-    app.listen(PORT, () => console.log('server is running on port: ', PORT));
+    app.listen(env.port, () => logger.info(`server is running on port: ${env.port}`));
 
 }
 
